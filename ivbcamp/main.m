1a9491859b17f9d7d43dd776884aefade3adfaf8
stirbeioctavian <octavian.stirbei@infrasoft.ro>
2014-05-20 13:51:44 +0300

    Initial commit from local git

---
+++ ivbcamp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ivbcamp
+//
+//  Created by Octavian Stirbei on 5/20/14.
+//  Copyright (c) 2014 Octavian Stirbei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
